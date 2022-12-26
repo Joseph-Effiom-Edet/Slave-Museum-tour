b761683ada60b200fece146ff074bcfc3de4b48b
Joseph Effiom Edet <90644500+AwesomeEJE@users.noreply.github.com>
2022-12-26 12:50:02 +0100

    Implement new font sizes and weights

--- total.js
+++ total.js
@@ -1,32 +1,38 @@
 var checkedTp = document.getElementById("sixth");
 var checkedtShirt = document.getElementById("seventh");
 var checkedHoodie = document.getElementById("eight");
+var checkedBoth = document.getElementById("eighted");
 var toTal = document.getElementById("total");
 var partyNumber = document.getElementById("partyNumber");
 var tourPackage = document.getElementById("tourPackage");
 var roundTrip = document.getElementById("roundTrip");
 var tShirtsPrice = document.getElementById("tShirtsPrice");
 var hoodiesPrice = document.getElementById("hoodiesPrice");
+var bothPrice = document.getElementById("bothPrice");
 var times = document.getElementsByName("times");
 
 var tp = 150;
 var tShirt = 35;
 var hoodie = 65;
+var both = 90;
 var total = 150;
 
 partyNumber.addEventListener("change", value);
 checkedTp.addEventListener("change", check);
 checkedtShirt.addEventListener("change", checkTshirt);
 checkedHoodie.addEventListener("change", checkHoodie);
+checkedBoth.addEventListener("change", checkBoth);
 
 function value() {
   var tot = 150;
   var ts = 35;
   var h = 65;
   var t = 150;
+  var b = 90;
   tp = t * this.value;
   tShirt = ts * this.value;
   hoodie = h * this.value;
+  both = b * this.value;
   total = tot * this.value;
   console.log(tp, tShirt, hoodie, total);
   tourPackage.innerHTML = `$${total}`;
@@ -35,6 +41,7 @@ function value() {
   times[1].innerHTML = `(x${this.value})`;
   times[2].innerHTML = `(x${this.value})`;
   times[3].innerHTML = `(x${this.value})`;
+  times[4].innerHTML = `(x${this.value})`;
 }
 
 function check() {
@@ -76,6 +83,20 @@ function checkHoodie() {
     total = total -= hoodie;
     toTal.innerHTML = `$${total}`;
     hoodiesPrice.innerHTML = `$${0}`;
+    console.log(total);
+    console.log("Unchecked");
+  }
+}
+function checkBoth() {
+  if (this.checked == true) {
+    total = total += both;
+    toTal.innerHTML = `$${total}`;
+    bothPrice.innerHTML = `$${both}`;
+    console.log(total);
+  } else {
+    total = total -= both;
+    toTal.innerHTML = `$${total}`;
+    bothPrice.innerHTML = `$${0}`;
     console.log(total);
     console.log("Unchecked");
   }
